57c2030ea3e4368150b0bb25888b70a415262695
Ansh Chaudhary <anshchaudhary025@gmail.com>
2024-07-04 11:05:12 +0530

    fixed speed issue by separating reading and writing

--- src/app/encryptDecrypt/crypto.cpp
+++ src/app/encryptDecrypt/crypto.cpp
@@ -2,71 +2,83 @@
 
 #include <chrono>
 #include <thread>
+#include <vector>
 
 #include "../fileHanding/readEnv.cpp"
 #include "../processes/task.hpp"
 #include "string.h"
 using namespace std;
 int executeBLOCK(std::shared_ptr<Task> task) {
-    // readEnv env;
     std::string key = task->keybuf;
     std::string replaceKey = "";
+    std::string s;
+    std::fstream f = std::move(task->fStream);
     const size_t len = key.size();
 
     int ind = 0;
     char ch;
 
-    while (task->fStream.get(ch)) {
+    while (f.get(ch)) {
         if (task->action == Action::ENCRYPT)
             replaceKey.push_back(ch);
-        ch ^= key[ind];
         if (task->action == Action::DECRYPT)
             replaceKey.push_back(ch);
-
+        s.push_back(ch ^= key[ind]);
         ind++, ind %= len;
         if (len == 0) key = replaceKey, replaceKey = "";
-        task->fStream.seekp(-1, std::ios::cur);
-        task->fStream.put(ch);
     }
-    task->fStream.close();
+    f.clear();
+    f.seekp(0, ios::beg);
+    for (auto &ch : s) f.put(ch);
+    f.close();
     return 0;
 }
 
 int executeCAESAR(std::shared_ptr<Task> task) {
     readEnv env;
     std::string key = env.getEnv();
+    std::string s;
+    std::fstream f = std::move(task->fStream);
+
     const int offset = key[0];
     char ch;
-    while (task->fStream.get(ch)) {
-        ch = (ch + offset) % 256;
-        task->fStream.seekp(-1, std::ios::cur);
-        task->fStream.put(ch);
-    }
-    task->fStream.close();
+
+    while (f.get(ch)) s.push_back((ch + offset) % 256);
+    f.clear();
+    f.seekp(0, ios::beg);
+    for (auto &ch : s) f.put(ch);
+    f.close();
     return 0;
 }
 
 int executeOTPAD(std::shared_ptr<Task> task) {
     std::string key = task->keybuf;
     const size_t len = key.size();
+    std::string s;
+    std::fstream f = std::move(task->fStream);
 
     int ind = 0;
     char ch;
 
-    while (task->fStream.get(ch)) {
-        ch ^= key[ind];
+    while (f.get(ch)) {
+        s.push_back(ch ^ key[ind]);
         ind++, ind %= len;
-        task->fStream.seekp(-1, std::ios::cur);
-        task->fStream.put(ch);
     }
-    task->fStream.close();
+
+    f.clear();
+    f.seekp(0, ios::beg);
+    for (auto &ch : s) f.put(ch);
+    f.close();
     return 0;
 }
 
 int executeVIGNERE(std::shared_ptr<Task> task) {
     readEnv env;
     std::string key = env.getEnv();
+    std::string s;
+    std::fstream f = std::move(task->fStream);
     const size_t len = key.size();
+
     int offset;
     char ch;
 
@@ -75,11 +87,10 @@ int executeVIGNERE(std::shared_ptr<Task> task) {
     else
         offset = 256;
 
-    while (task->fStream.get(ch)) {
-        ch = key[ch + offset];
-        task->fStream.seekp(-1, std::ios::cur);
-        task->fStream.put(ch);
-    }
-    task->fStream.close();
+    while (f.get(ch)) s.push_back(key[ch + offset]);
+    f.clear();
+    f.seekp(0, ios::beg);
+    for (auto &ch : s) f.put(ch);
+    f.close();
     return 0;
 }
--- src/app/processes/task.hpp
+++ src/app/processes/task.hpp
@@ -39,26 +39,6 @@ struct Task {
                                            : "method=VIGNERE");
         return oss.str();
     }
-
-    // static std::shared_ptr<Task> fromString(const std::string filePath, std::string actionStr, std::string cipherStr) {
-    //     if (filePath.size() && actionStr.size() && cipherStr.size()) {
-    //         Action action = actionStr == "encrypt" ? Action::ENCRYPT : Action::DECRYPT;
-
-    //         Cipher cipher = cipherStr == "otp"      ? Cipher::OTPAD
-    //                         : cipherStr == "block"  ? Cipher::BLOCK
-    //                         : cipherStr == "caesar" ? Cipher::CAESAR
-    //                                                 : Cipher::VIGNERE;
-    //         IO io(filePath);
-    //         std::fstream fileStream = io.getFileStream();
-    //         if (fileStream.is_open()) {
-    //             return std::make_unique<Task>(std::move(fileStream), filePath, action, cipher);
-    //         } else {
-    //             throw std::runtime_error("Failed to open file: " + filePath);
-    //         }
-    //     } else {
-    //         throw std::runtime_error("Invalid Task Data Format");
-    //     }
-    // }
 };
 
 #endif
